4fe953af809665dd5a1e550394cc0ebb22a34294
dhee <dheemanthj.bs23@bmsce.ac.in>
2026-05-13 15:05:02 +0530

    init(api-service/main): add Java project files

---
+++ README.md
@@ -0,0 +1,4 @@
+# api-service
+Branch: `main`
+Language: Java
+Managed by Jenkins CI/CD Simulator.
---
+++ src/main/java/Application.java
@@ -0,0 +1,5 @@
+public class Application {
+  public static void main(String[] args) {
+    System.out.println("api-service running on branch: main");
+  }
+}
